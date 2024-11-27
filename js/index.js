c1361b8946e82a429d1217442ab9a22ebca66b8f
abduvohid-jck <abduvohidmurodov26@gmail.com>
2024-11-27 15:35:25 +0500

    finished

---
+++ js/index.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+      width: {
+        DEFAULT: "100%",
+        tablet: "600px",
+        largetablet: "960px",
+        desktop: "1200px",
+      },
+      padding: {
+        DEFAULT: "20px",
+      },
+    },
+    fontFamily: {
+      lato: "lato",
+      raleway: "raleway",
+    },
+    screens: {
+      tablet: "640px",
+      largetablet: "1000px",
+      desktop: "1240px",
+    },
+  },
+};
